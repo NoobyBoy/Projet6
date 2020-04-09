6a63d7b9cb46299aece5b69cdb95baa645360913
Hugo Taieb <hugotaieb@portosxhugo.taieb.fr>
2020-04-09 14:51:54 +0200

    server folder

---
+++ server/main.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const dataRequest = require('./src/dataRequest');
+const affinater = require('./src/affinateResults')
+var EventEmitter = require('events').EventEmitter;
+var results = [];
+var attempt = 1;
+
+var app = express();
+
+var em = new EventEmitter();
+
+dataRequest.scrapData(em);
+em.on('scraped', function(result) {
+    results = result;
+    affinater.missingCountries(results);
+    var em2 = new EventEmitter();
+    dataRequest.scrapDataHistory(em2, results);
+    em2.on('scraped', function(result) {
+        affinater.affinate(results)
+    })
+    em2.on('scrapeFailRequest', function(result){
+        throw result
+    });
+    em2.on('scrapeFailFile', function(result){
+        throw result
+    });
+});
+em.on('scrapeFailUrl', function(result) {
+    console.log(result + "'s Data is not yet available")
+    var d = new Date();
+    d.setDate(d.getDate() - attempt);
+    attempt++;
+    if (attempt == 10) throw "Problem with the url or the date"
+    dataRequest.scrapData(em, d);
+});
+em.on('scrapeFailRequest', function(result){
+    throw result
+});
+em.on('scrapeFailFile', function(result){
+    throw result
+});
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Routes
+////////////////////////////////////////////////////////////////////////////////
+
+app.get('/', function(req, res) {
+  res.render('test.ejs', {resu: results});
+});
+
+app.get('/data/countries', function(req, res) {
+    if (results.Country) res.json({status : 200, data : results.Country});
+    else res.json({status : 400, data : []})
+});
+
+app.get('/data/countries/:country', function(req, res) {
+    if (req.params.country) {
+        country = req.params.country[0].toUpperCase() + req.params.country.slice(1).toLowerCase();
+        if (results.Country[country]) res.json({status : 200, data : results.Country[country]});
+        else res.json({status : 400, data : []})
+    } else res.json({status : 400, data : []})
+});
+
+app.get('/data/zones', function(req, res) {
+    if (results.Zone) res.json({status : 200, data : results.Zone});
+    else res.json({status : 400, data : []})
+});
+
+app.get('/data/zones/:zone', function(req, res) {
+    if (req.params.zone) {
+        zone = req.params.zone[0].toUpperCase() + req.params.zone.slice(1).toLowerCase();
+        if (results.Zone[zone]) res.json({status : 200, data : results.Zone[zone]});
+        else res.json({status : 400, data : []})
+    } else res.json({status : 400, data : []})
+});
+
+app.get('/data/cities', function(req, res) {
+    if (results.City) res.json({status : 200, data : results.City});
+    else res.json({status : 400, data : []})
+});
+
+app.get('/data/cities/:city', function(req, res) {
+    if (req.params.city) {
+        city = req.params.city[0].toUpperCase() + req.params.city.slice(1).toLowerCase();
+        if (results.City[city]) res.json({status : 200, data : results.City[city]});
+        else res.json({status : 400, data : []})
+    } else res.json({status : 400, data : []})
+});
+
+app.listen(8080);
---
+++ server/src/dataRequest.js
@@ -0,0 +1,130 @@
+const https = require('https');
+const fs = require('fs');
+const csv = require('csv-parser');
+const dateFormater = require('./dateFormater');
+var EventEmitter = require('events').EventEmitter;
+
+const GetErr = '404: Not Found'
+
+
+
+exports.scrapData = function(em, date=null)  {
+    //Initialisation of url and date etc...
+    base_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'
+    const country = {};
+    const zone = {};
+    const city = {};
+    var today = ''
+    if (!date) {
+        var d = new Date();
+        today = dateFormater.mm_dd_yyyy(d)
+    } else {
+        today = dateFormater.mm_dd_yyyy(date);
+    }
+    fs.writeFile('./data/' + today + '.csv', '', function() {});
+    process.stdout.write("Begining of the Request to " + base_url + today + ".csv ");
+
+    //Begining of the request
+    https.get(base_url + today + ".csv", (resp) => {
+      let data = '';
+      // A chunk of data has been recieved. we copy it into the corresponding file
+      resp.on('data', (chunk) => {
+        process.stdout.write(".");
+        data += chunk
+        fs.appendFile('./data/' + today + '.csv', chunk, function (err) {
+            if (err) em.emit('scrapeFailFile', err.message);
+        });
+      });
+
+      // The whole response has been received.
+      resp.on('end', () => {
+          console.log(" Request Done")
+          if (data == GetErr) {
+              em.emit('scrapeFailUrl', today);
+              return;
+          }
+          // we parse it
+          process.stdout.write("Starting parsing of the csv File ...")
+          fs.createReadStream('./data/' + today + '.csv')
+            .pipe(csv())
+            .on('data', (data) => {
+                if (!data.Admin2 && !data.Province_State) country[data.Country_Region] = data;
+                else if (!data.Admin2) zone[data.Province_State] = data
+                else city[data.Admin2] = data;
+            })
+            .on('end', () => {
+                console.log(" Parsing Done")
+                em.emit('scraped', {Country : country, Zone : zone, City : city});
+            });
+      });
+
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+      em.emit('scrapeFailRequest', "Error: " + err.message);
+    });
+}
+
+
+getHistory = function(data) {
+    today = new Date();
+    firstDay = new Date("January 22, 2020");
+
+    history = [];
+    while (dateFormater.m_d_yy(today) != dateFormater.m_d_yy(firstDay)) {
+        history.push(data[dateFormater.m_d_yy(firstDay)])
+        firstDay.setDate(firstDay.getDate() + 1)
+    }
+    return(history);
+}
+
+exports.scrapDataHistory = function(em, res)  {
+    //Initialisation of url and date etc...
+
+    base_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+    const country = {};
+    const zone = {};
+
+    fs.writeFile('History.csv', '', function() {});
+    process.stdout.write("Begining of the Request to " + base_url);
+
+    //Begining of the request
+    https.get(base_url, (resp) => {
+      let data = '';
+      // A chunk of data has been recieved. we copy it into the corresponding file
+      resp.on('data', (chunk) => {
+        process.stdout.write(".");
+        data += chunk
+        var c = ''
+        c += chunk
+
+        fs.appendFile('./data/' + 'History.csv', c.replace(/\//g, "_"), function (err) {
+            if (err) em.emit('scrapeFailFile', err.message);
+        });
+      });
+
+      // The whole response has been received.
+      resp.on('end', () => {
+          console.log(" Request Done")
+          if (data == GetErr) {
+              em.emit('scrapeFailUrl', today);
+              return;
+          }
+          // we parse it
+          process.stdout.write("Starting parsing of the csv File ...")
+          fs.createReadStream('./data/' + 'History.csv')
+            .pipe(csv())
+            .on('data', (data) => {
+                if (!data.Province_State && res.Country[data.Country_Region]) res.Country[data.Country_Region].History = getHistory(data);
+                else if (res.Zone[data.Province_State]) res.Zone[data.Province_State].History = getHistory(data);
+            })
+            .on('end', () => {
+                console.log(" Parsing Done")
+                em.emit('scraped', null);
+            });
+      });
+
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+      em.emit('scrapeFailRequest', "Error: " + err.message);
+    });
+}
