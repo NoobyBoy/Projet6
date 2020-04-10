3d9b82afcca6e281384dfd25a91ae884a269bf67
Cavalli Timothée <timothee.cavalli@epitech.eu>
2020-04-10 14:42:28 +0200

    Result improvement

--- server/main.js
+++ server/main.js
@@ -6,6 +6,7 @@ const express = require('express');
 var app = express();
 var EventEmitter = require('events').EventEmitter;
 var results = [];
+var attempt = 1;
 
 
 
@@ -14,11 +15,19 @@ var em = new EventEmitter();
 dataRequest.scrapData(em);
 em.on('scraped', function(result) {
     results = result;
+    if (Object.keys(results.Country).length < 90|| Object.keys(results.Zone).length < 43 || Object.keys(results.City).length < 700) {
+        console.log("Invalid data : new attempt to get it.");
+        console.log(Object.keys(results.Country).length + " " + Object.keys(results.Zone).length + " " + Object.keys(results.City).length)
+        attempt = 1;
+        dataRequest.scrapData(em);
+        return;
+    }
     affinater.missingCountries(results);
     var em2 = new EventEmitter();
     dataRequest.scrapDataHistory(em2, results);
     em2.on('scraped', function(result) {
-        affinater.affinate(results)
+        affinater.affinate(results);
+        affinater.Total(results);
     })
     em2.on('scrapeFailRequest', function(result){
         throw result
@@ -27,7 +36,6 @@ em.on('scraped', function(result) {
         throw result
     });
 });
-var attempt = 1;
 em.on('scrapeFailUrl', function(result) {
     console.log(result + "'s Data is not yet available")
     var d = new Date();
@@ -47,6 +55,16 @@ em.on('scrapeFailFile', function(result){
 // Routes                                                                     //
 ////////////////////////////////////////////////////////////////////////////////
 
+function toUpper(str) {
+return str
+    .toLowerCase()
+    .split(' ')
+    .map(function(word) {
+        return word[0].toUpperCase() + word.substr(1);
+    })
+    .join(' ');
+ }
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -56,43 +74,43 @@ app.use((req, res, next) => {
 
 
 app.get('/data/countries', function(req, res) {
-    if (results.Country) res.json({status : 200, data : results.Country});
-    else res.json({status : 400, data : []})
+    if (results.Country) res.status(200).json({status : 200, data : results.Country});
+    else res.status(400).json({status : 400, data : []})
 });
 
 app.get('/data/countries/:country', function(req, res) {
     if (req.params.country) {
-        country = req.params.country[0].toUpperCase() + req.params.country.slice(1).toLowerCase();
+        country = toUpper(req.params.country)
         if (country == "Us") country = "US";
-        if (results.Country[country]) res.json({status : 200, data : results.Country[country]});
-        else res.json({status : 400, data : []})
-    } else res.json({status : 400, data : []})
+        if (results.Country[country]) res.status(200).json({status : 200, data : results.Country[country]});
+        else res.status(400).json({status : 400, data : []})
+    } else res.status(400).json({status : 400, data : []})
 });
 
 app.get('/data/zones', function(req, res) {
-    if (results.Zone) res.json({status : 200, data : results.Zone});
-    else res.json({status : 400, data : []})
+    if (results.Zone) res.status(200).json({status : 200, data : results.Zone});
+    else res.status(400).json({status : 400, data : []})
 });
 
 app.get('/data/zones/:zone', function(req, res) {
     if (req.params.zone) {
-        zone = req.params.zone[0].toUpperCase() + req.params.zone.slice(1).toLowerCase();
-        if (results.Zone[zone]) res.json({status : 200, data : results.Zone[zone]});
-        else res.json({status : 400, data : []})
-    } else res.json({status : 400, data : []})
+        zone = toUpper(req.params.zone)
+        if (results.Zone[zone]) res.status(200).json({status : 200, data : results.Zone[zone]});
+        else res.status(400).json({status : 400, data : []})
+    } else res.status(400).json({status : 400, data : []})
 });
 
 app.get('/data/cities', function(req, res) {
-    if (results.City) res.json({status : 200, data : results.City});
-    else res.json({status : 400, data : []})
+    if (results.City) res.status(200).json({status : 200, data : results.City});
+    else res.status(400).json({status : 400, data : []})
 });
 
 app.get('/data/cities/:city', function(req, res) {
     if (req.params.city) {
-        city = req.params.city[0].toUpperCase() + req.params.city.slice(1).toLowerCase();
-        if (results.City[city]) res.json({status : 200, data : results.City[city]});
-        else res.json({status : 400, data : []})
-    } else res.json({status : 400, data : []})
+        city = toUpper(req.params.city)
+        if (results.City[city]) res.status(200).json({status : 200, data : results.City[city]});
+        else res.status(400).json({status : 400, data : []})
+    } else res.status(400).json({status : 400, data : []})
 });
 
 app.set('port', 8080);
--- server/src/affinateResults.js
+++ server/src/affinateResults.js
@@ -5,26 +5,102 @@ exports.missingCountries = function(res) {
     res.Country["US"].Lat = "39.381266";
     res.Country["US"].Long_ = "-97.922211";
     res.Country["US"].Combined_Key = "US";
+    res.Country["US"].Confirmed = 0;
+    res.Country["US"].Deaths = 0;
+    res.Country["US"].Recovered =0;
+    res.Country["US"].Active = 0;
     res.Country["US"].History = []
     res.Country["China"] = Object.assign({}, res.Country["France"]);
     res.Country["China"].Country_Region = "China";
     res.Country["China"].Lat = "35.486703";
     res.Country["China"].Long_ = "101.901875";
     res.Country["China"].Combined_Key = "China";
+    res.Country["China"].Confirmed = 0;
+    res.Country["China"].Deaths = 0;
+    res.Country["China"].Recovered =0;
+    res.Country["China"].Active = 0;
     res.Country["China"].History = []
 
     console.log("Done");
 }
 
 exports.affinate = function(res) {
-    fu = true;
+    fc = true;
     process.stdout.write("Begining of the affination ... ");
+    // other than History -> US - CHina
     for (key in res.Zone) {
         data = res.Zone[key];
-        if (data.Country_Region == "China" && data.History) {
-            if (fu == true) {res.Country["China"].History = Array.from(data.History); fu = false; continue}
+        if (data.Country_Region == "China") {
+            if (fc == true && data.History) {
+                res.Country["China"].History = new Array(data.History.length).fill(0);
+                fc = false;
+            }
+            res.Country["China"].Confirmed = parseInt(res.Country["China"].Confirmed, 10) + parseInt(data.Confirmed, 10);
+            res.Country["China"].Deaths =  parseInt(res.Country["China"].Deaths, 10) + parseInt(data.Deaths, 10);
+            res.Country["China"].Recovered =  parseInt(res.Country["China"].Recovered, 10) + parseInt(data.Recovered, 10);
+            res.Country["China"].Active =  parseInt(res.Country["China"].Active, 10) + parseInt(data.Active, 10);
+            if (data.History) {
+                for (let i = 0; i < data.History.length; ++i) {
+                    res.Country["China"].History[i] = parseInt(res.Country["China"].History[i], 10) + parseInt(data.History[i], 10);
+                }
+            }
+        }
+        if (data.Country_Region == "US") {
+            res.Country["US"].Confirmed = parseInt(res.Country["US"].Confirmed, 10) + parseInt(data.Confirmed, 10);
+            res.Country["US"].Deaths =  parseInt(res.Country["US"].Deaths, 10) + parseInt(data.Deaths, 10);
+            res.Country["US"].Recovered =  parseInt(res.Country["US"].Recovered, 10) + parseInt(data.Recovered, 10);
+            res.Country["US"].Active =  parseInt(res.Country["US"].Active, 10) + parseInt(data.Active, 10);
+        }
+    }
+    for (key in res.City) {
+        data = res.City[key];
+        if (data.Country_Region == "US") {
+            res.Country["US"].Confirmed = parseInt(res.Country["US"].Confirmed, 10) + parseInt(data.Confirmed, 10);
+            res.Country["US"].Deaths =  parseInt(res.Country["US"].Deaths, 10) + parseInt(data.Deaths, 10);
+            res.Country["US"].Recovered =  parseInt(res.Country["US"].Recovered, 10) + parseInt(data.Recovered, 10);
+            res.Country["US"].Active =  parseInt(res.Country["US"].Active, 10) + parseInt(data.Active, 10);
+        }
+    }
+    console.log("Done");
+}
+
+exports.Total = function(res) {
+    process.stdout.write("Begining of total calculation ... ");
+    res.Country["Total"] = Object.assign({}, res.Country["France"]);
+    res.Country["Total"].Country_Region = "Total";
+    res.Country["Total"].Lat = "0";
+    res.Country["Total"].Long_ = "0";
+    res.Country["Total"].Combined_Key = "Total";
+    res.Country["Total"].Confirmed = 0;
+    res.Country["Total"].Deaths = 0;
+    res.Country["Total"].Recovered =0;
+    res.Country["Total"].Active = 0;
+    res.Country["Total"].History = new Array(res.Country["France"].History.length).fill(0);
+    for (key in res.Country) {
+        data = res.Country[key];
+        if (data.Country_Region != "Total") {
+            res.Country["Total"].Confirmed = parseInt(res.Country["Total"].Confirmed, 10) + parseInt(data.Confirmed, 10);
+            res.Country["Total"].Deaths =  parseInt(res.Country["Total"].Deaths, 10) + parseInt(data.Deaths, 10);
+            res.Country["Total"].Recovered =  parseInt(res.Country["Total"].Recovered, 10) + parseInt(data.Recovered, 10);
+            res.Country["Total"].Active =  parseInt(res.Country["Total"].Active, 10) + parseInt(data.Active, 10);
+            if (data.History) {
+                for (let i = 0; i < data.History.length; ++i) {
+                    res.Country["Total"].History[i] = parseInt(res.Country["Total"].History[i], 10) + parseInt(data.History[i], 10);
+                }
+            }
+        }
+    }
+    for (key in res.Zone) {
+        data = res.Zone[key];
+        if (data.Country_Region != "China" && data.Country_Region != "US") {
+            res.Country["Total"].Confirmed = parseInt(res.Country["Total"].Confirmed, 10) + parseInt(data.Confirmed, 10);
+            res.Country["Total"].Deaths =  parseInt(res.Country["Total"].Deaths, 10) + parseInt(data.Deaths, 10);
+            res.Country["Total"].Recovered =  parseInt(res.Country["Total"].Recovered, 10) + parseInt(data.Recovered, 10);
+            res.Country["Total"].Active =  parseInt(res.Country["Total"].Active, 10) + parseInt(data.Active, 10);
+        }
+        if (data.History && data.Country_Region != "China") {
             for (let i = 0; i < data.History.length; ++i) {
-                res.Country["China"].History[i] = parseInt(res.Country["China"].History[i], 10) + parseInt(data.History[i], 10);
+                res.Country["Total"].History[i] = parseInt(res.Country["Total"].History[i], 10) + parseInt(data.History[i], 10);
             }
         }
     }
