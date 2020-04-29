8d893ef21c009e4e83009a416690130d617153fa
dguillaume <guillaume.dussourd@epitech.eu>
2020-04-29 15:10:18 +0200

    adding css 3

--- client/src/components/LastUpdate.vue
+++ client/src/components/LastUpdate.vue
@@ -1,8 +1,8 @@
 <template>
-<div id=Day>
-<h1>Last Updated at (M/D/YYYY)</h1>
-<p>{{this.method()}}</p>
-</div>
+<v-flex xs12 md3 style="max-height: 500px; background: #808080;">
+<h1 >Last Updated at (M/D/YYYY)</h1>
+<p style="font-size: xx-large; color: red">{{this.method()}}</p>
+</v-flex>
 </template>
 
 <script>
