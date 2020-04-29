c673c86f5e55fc50f761b4707407b87465fba8fc
dguillaume <guillaume.dussourd@epitech.eu>
2020-04-29 15:55:36 +0200

    css test

--- client/src/components/LastUpdate.vue
+++ client/src/components/LastUpdate.vue
@@ -1,7 +1,11 @@
 <template>
 <v-flex xs12 md3 style="max-height: 500px; background: #808080;">
-<h1 >Last Updated at (M/D/YYYY)</h1>
-<p style="font-size: xx-large; color: red">{{this.method()}}</p>
+  <v-row justify="center" align="center">
+    <h1>Last Updated at (M/D/YYYY)</h1>
+  </v-row>
+  <v-row justify="center" align="center">
+    <p style="font-size: xx-large; color: red">{{this.method()}}</p>
+  </v-row>
 </v-flex>
 </template>
 
