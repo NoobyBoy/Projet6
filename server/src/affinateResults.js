833732cbb70d1d72a568bb993b3605d92b19167b
Cavalli Timothée <timothee.cavalli@epitech.eu>
2020-04-30 11:24:38 +0200

    total is ok

--- server/src/affinateResults.js
+++ server/src/affinateResults.js
@@ -1,5 +1,10 @@
 const dateFormater = require('./dateFormater');
 
+function roughScale(x) {
+  const parsed = parseInt(x, 10);
+  if (isNaN(parsed)) { return 0 }
+  return parsed;
+}
 
 exports.testResult = function(res) {
     process.stdout.write("Testing the integrity of the data ... ");
@@ -56,30 +61,30 @@ exports.affinate = function(res) {
                 res.Country["China"].History = new Array(data.History.length).fill(0);
                 fc = false;
             }
-            res.Country["China"].Confirmed = parseInt(res.Country["China"].Confirmed, 10) + parseInt(data.Confirmed, 10);
-            res.Country["China"].Deaths =  parseInt(res.Country["China"].Deaths, 10) + parseInt(data.Deaths, 10);
-            res.Country["China"].Recovered =  parseInt(res.Country["China"].Recovered, 10) + parseInt(data.Recovered, 10);
-            res.Country["China"].Active =  parseInt(res.Country["China"].Active, 10) + parseInt(data.Active, 10);
+            res.Country["China"].Confirmed = roughScale(res.Country["China"].Confirmed, 10) + roughScale(data.Confirmed, 10);
+            res.Country["China"].Deaths =  roughScale(res.Country["China"].Deaths, 10) + roughScale(data.Deaths, 10);
+            res.Country["China"].Recovered =  roughScale(res.Country["China"].Recovered, 10) + roughScale(data.Recovered, 10);
+            res.Country["China"].Active =  roughScale(res.Country["China"].Active, 10) + roughScale(data.Active, 10);
             if (data.History) {
                 for (let i = 0; i < data.History.length; ++i) {
-                    res.Country["China"].History[i] = parseInt(res.Country["China"].History[i], 10) + parseInt(data.History[i], 10);
+                    res.Country["China"].History[i] = roughScale(res.Country["China"].History[i], 10) + roughScale(data.History[i], 10);
                 }
             }
         }
         if (data.Country_Region == "US") {
-            res.Country["US"].Confirmed = parseInt(res.Country["US"].Confirmed, 10) + parseInt(data.Confirmed, 10);
-            res.Country["US"].Deaths =  parseInt(res.Country["US"].Deaths, 10) + parseInt(data.Deaths, 10);
-            res.Country["US"].Recovered =  parseInt(res.Country["US"].Recovered, 10) + parseInt(data.Recovered, 10);
-            res.Country["US"].Active =  parseInt(res.Country["US"].Active, 10) + parseInt(data.Active, 10);
+            res.Country["US"].Confirmed = roughScale(res.Country["US"].Confirmed, 10) + roughScale(data.Confirmed, 10);
+            res.Country["US"].Deaths =  roughScale(res.Country["US"].Deaths, 10) + roughScale(data.Deaths, 10);
+            res.Country["US"].Recovered =  roughScale(res.Country["US"].Recovered, 10) + roughScale(data.Recovered, 10);
+            res.Country["US"].Active =  roughScale(res.Country["US"].Active, 10) + roughScale(data.Active, 10);
         }
     }
     for (key in res.City) {
         data = res.City[key];
         if (data.Country_Region == "US") {
-            res.Country["US"].Confirmed = parseInt(res.Country["US"].Confirmed, 10) + parseInt(data.Confirmed, 10);
-            res.Country["US"].Deaths =  parseInt(res.Country["US"].Deaths, 10) + parseInt(data.Deaths, 10);
-            res.Country["US"].Recovered =  parseInt(res.Country["US"].Recovered, 10) + parseInt(data.Recovered, 10);
-            res.Country["US"].Active =  parseInt(res.Country["US"].Active, 10) + parseInt(data.Active, 10);
+            res.Country["US"].Confirmed = roughScale(res.Country["US"].Confirmed, 10) + roughScale(data.Confirmed, 10);
+            res.Country["US"].Deaths =  roughScale(res.Country["US"].Deaths, 10) + roughScale(data.Deaths, 10);
+            res.Country["US"].Recovered =  roughScale(res.Country["US"].Recovered, 10) + roughScale(data.Recovered, 10);
+            res.Country["US"].Active =  roughScale(res.Country["US"].Active, 10) + roughScale(data.Active, 10);
         }
     }
     console.log("Done");
@@ -100,13 +105,14 @@ exports.Total = function(res) {
     for (key in res.Country) {
         data = res.Country[key];
         if (data.Country_Region != "Total") {
-            res.Country["Total"].Confirmed = parseInt(res.Country["Total"].Confirmed, 10) + parseInt(data.Confirmed, 10);
-            res.Country["Total"].Deaths =  parseInt(res.Country["Total"].Deaths, 10) + parseInt(data.Deaths, 10);
-            res.Country["Total"].Recovered =  parseInt(res.Country["Total"].Recovered, 10) + parseInt(data.Recovered, 10);
-            res.Country["Total"].Active =  parseInt(res.Country["Total"].Active, 10) + parseInt(data.Active, 10);
+            res.Country["Total"].Confirmed = roughScale(res.Country["Total"].Confirmed, 10) + roughScale(data.Confirmed, 10);
+            res.Country["Total"].Deaths =  roughScale(res.Country["Total"].Deaths, 10) + roughScale(data.Deaths, 10);
+            res.Country["Total"].Recovered =  roughScale(res.Country["Total"].Recovered, 10) + roughScale(data.Recovered, 10);
+            res.Country["Total"].Active =  roughScale(res.Country["Total"].Active, 10) + roughScale(data.Active, 10);
             if (data.History) {
                 for (let i = 0; i < data.History.length; ++i) {
-                    res.Country["Total"].History[i] = parseInt(res.Country["Total"].History[i], 10) + parseInt(data.History[i], 10);
+                    res.Country["Total"].History[i] = roughScale(res.Country["Total"].History[i], 10) + roughScale(data.History[i], 10);
+                    console.log(res.Country["Total"].History[i]);
                 }
             }
         }
@@ -114,14 +120,14 @@ exports.Total = function(res) {
     for (key in res.Zone) {
         data = res.Zone[key];
         if (data.Country_Region != "China" && data.Country_Region != "US") {
-            res.Country["Total"].Confirmed = parseInt(res.Country["Total"].Confirmed, 10) + parseInt(data.Confirmed, 10);
-            res.Country["Total"].Deaths =  parseInt(res.Country["Total"].Deaths, 10) + parseInt(data.Deaths, 10);
-            res.Country["Total"].Recovered =  parseInt(res.Country["Total"].Recovered, 10) + parseInt(data.Recovered, 10);
-            res.Country["Total"].Active =  parseInt(res.Country["Total"].Active, 10) + parseInt(data.Active, 10);
+            res.Country["Total"].Confirmed = roughScale(res.Country["Total"].Confirmed, 10) + roughScale(data.Confirmed, 10);
+            res.Country["Total"].Deaths =  roughScale(res.Country["Total"].Deaths, 10) + roughScale(data.Deaths, 10);
+            res.Country["Total"].Recovered =  roughScale(res.Country["Total"].Recovered, 10) + roughScale(data.Recovered, 10);
+            res.Country["Total"].Active =  roughScale(res.Country["Total"].Active, 10) + roughScale(data.Active, 10);
         }
         if (data.History && data.Country_Region != "China") {
             for (let i = 0; i < data.History.length; ++i) {
-                res.Country["Total"].History[i] = parseInt(res.Country["Total"].History[i], 10) + parseInt(data.History[i], 10);
+                res.Country["Total"].History[i] = roughScale(res.Country["Total"].History[i], 10) + roughScale(data.History[i], 10);
             }
         }
     }
